bde0428f7a1cc7c4f4b733d6c314d2e9769c495f
Yura Chornyi <chorny.yura@gmail.com>
2025-04-23 17:10:44 +0300

    feat: add views for Profile model

--- social/views.py
+++ social/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from social.models import Profile
+from social.permissions import IsAdminOrOwnerOrReadOnly
+from social.serializers import (
+    ProfileSerializer,
+    ProfileCreateSerializer,
+    ProfileListSerializer,
+)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.select_related("user")
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAdminOrOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ProfileCreateSerializer
+        if self.action == "list":
+            return ProfileListSerializer
+
+        return ProfileSerializer
