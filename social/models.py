0042573f9766bef9c80ac4028ecb6ac5bddbad44
Yura Chornyi <chorny.yura@gmail.com>
2025-04-23 13:55:33 +0300

    feat: add Profile model for user

--- social/models.py
+++ social/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+
+def profile_image_file_path(instance: "Profile", filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.full_name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/profiles/", filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    bio = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(
+        upload_to=profile_image_file_path, null=True, blank=True
+    )
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    def __str__(self):
+        return self.full_name
