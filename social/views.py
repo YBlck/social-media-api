89f7438c00118589029df6cd354bdf8f953fd5ab
Yura Chornyi <chorny.yura@gmail.com>
2025-04-23 18:06:24 +0300

    feat: add filtering for profiles by name, country or city

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
+from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from social.models import Profile
@@ -17,6 +17,26 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.select_related("user")
     serializer_class = ProfileSerializer
     permission_classes = (IsAdminOrOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        """Profile filtering by user first or last name, country or city"""
+        name = self.request.query_params.get("name")
+        country = self.request.query_params.get("country")
+        city = self.request.query_params.get("city")
+
+        queryset = self.queryset
+
+        if name:
+            queryset = queryset.filter(
+                Q(user__first_name__icontains=name)
+                | Q(user__last_name__icontains=name)
+            )
+        if country:
+            queryset = queryset.filter(country__icontains=country)
+        if city:
+            queryset = queryset.filter(city__icontains=city)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -42,7 +62,9 @@ class ProfileViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         elif request.method in ["PUT", "PATCH"]:
-            serializer = self.get_serializer(profile, data=request.data, partial=(request.method == "PATCH"))
+            serializer = self.get_serializer(
+                profile, data=request.data, partial=(request.method == "PATCH")
+            )
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
