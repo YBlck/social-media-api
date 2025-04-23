9d8659b9a8e118f69c90e807d24104e44d9a966d
Yura Chornyi <chorny.yura@gmail.com>
2025-04-23 13:43:36 +0300

    feat: make first_name and last_name required fields in the user model

--- user/models.py
+++ user/models.py
@@ -46,8 +46,10 @@ class UserManager(DjangoUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
