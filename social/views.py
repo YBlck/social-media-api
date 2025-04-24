f699d5982eb19ab384ea2416f987f868c519a93e
Yura Chornyi <chorny.yura@gmail.com>
2025-04-24 13:58:01 +0300

    feat: add ViewSet for posts

--- social/views.py
+++ social/views.py
@@ -1,4 +1,3 @@
-from django.core.serializers import get_serializer
 from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -6,7 +5,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from social.models import Profile, Follow
+from social.models import Profile, Follow, Post
 from social.permissions import IsAdminOrOwnerOrReadOnly
 from social.serializers import (
     ProfileSerializer,
@@ -15,6 +14,9 @@ from social.serializers import (
     FollowUnfollowSerializer,
     FollowersSerializer,
     FollowingSerializer,
+    PostSerializer,
+    PostCreateUpdateSerializer,
+    PostListSerializer,
 )
 
 
@@ -90,6 +92,13 @@ class ProfileViewSet(viewsets.ModelViewSet):
     def follow(self, request, pk=None):
         """Start following a user"""
         profile_to_follow = self.get_object()
+
+        if not Profile.objects.filter(user=request.user).exists():
+            return Response(
+                {"message": "You have to create a profile first"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user_to_follow = request.user.profile
 
         subscription = Follow.objects.filter(
@@ -124,6 +133,13 @@ class ProfileViewSet(viewsets.ModelViewSet):
     def unfollow(self, request, pk=None):
         """Stop following a user"""
         profile_to_unfollow = self.get_object()
+
+        if not Profile.objects.filter(user=request.user).exists():
+            return Response(
+                {"message": "You have to create a profile first"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user_to_unfollow = request.user.profile
 
         subscription = Follow.objects.filter(
@@ -145,7 +161,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
             status=status.HTTP_204_NO_CONTENT,
         )
 
-    @action(detail=True, methods=["GET"], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=["GET"])
     def followers(self, request, pk=None):
         """List of all the user's followers"""
         profile = self.get_object()
@@ -153,10 +169,32 @@ class ProfileViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(followers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=["GET"], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=["GET"])
     def following(self, request, pk=None):
         """List of all user subscriptions"""
         profile = self.get_object()
         following = profile.following.select_related("following__user")
         serializer = self.get_serializer(following, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAdminOrOwnerOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        if self.action in ["create", "update", "partial_update"]:
+            return PostCreateUpdateSerializer
+        return PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        profile = get_object_or_404(Profile, user=request.user)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save(profile=profile)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
