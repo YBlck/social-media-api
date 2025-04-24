e99e3ffe5133017edc2e56f73e2cf6da1cab5b85
Yura Chornyi <chorny.yura@gmail.com>
2025-04-24 13:58:11 +0300

    feat: add router for posts

--- social/urls.py
+++ social/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from social.views import ProfileViewSet
+from social.views import ProfileViewSet, PostViewSet
 
 app_name = "social"
 
 router = DefaultRouter()
 router.register("profiles", ProfileViewSet, basename="profile")
+router.register("posts", PostViewSet, basename="post")
 
 urlpatterns = [
     path("", include(router.urls)),
